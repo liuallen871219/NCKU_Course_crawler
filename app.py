fd5740e1b8fbbd2560993fd243ae2dfcd1395e8c
liuallen871219 <40296275+liuallen871219@users.noreply.github.com>
2019-12-07 20:37:47 +0800

    finish

---
+++ app.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import requests
+from flask import Flask, jsonify, request, abort, send_file
+from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from bs4 import BeautifulSoup
+from fsm import TocMachine
+from utils import send_text_message
+
+load_dotenv()
+
+
+machine = TocMachine(
+    states=["user", "state1", "state2","show_fsm"],
+    transitions=[
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "state1",
+            "conditions": "is_going_to_state1",
+        },
+        {
+            "trigger": "advance",
+            "source": "state1",
+            "dest": "state2",
+            "conditions": "is_going_to_state2",
+        },
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "show_fsm",
+            "conditions": "is_going_to_show_fsm",
+        },
+         {
+            "trigger": "advance",
+            "source": "state1",
+            "dest": "user",
+            "conditions": "is_going_to_user",
+        },
+        #{"trigger": "go_back", "source": "state1", "dest": "user"},
+        {"trigger": "go_back_state1", "source": "state2", "dest": "state1"},
+        {"trigger": "go_back_user", "source": "show_fsm", "dest": "user"}
+    ],
+    initial="user",
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+app = Flask(__name__, static_url_path="")
+
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+if channel_secret is None:
+    print("Specify LINE_CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+if channel_access_token is None:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text)
+        )
+
+    return "OK"
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook_handler():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info(f"Request body: {body}")
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        if not isinstance(event.message.text, str):
+            continue
+        print(f"\nFSM STATE: {machine.state}")
+        print(f"REQUEST BODY: \n{body}")
+        response = machine.advance(event)
+        if response == False:
+            send_text_message(event.reply_token, "Not Entering any State")
+
+    return "OK"
+
+
+@app.route("/show-fsm", methods=["GET"])
+def show_fsm():
+    machine.get_graph().draw("my_fsm.png", prog="dot", format="png")
+    return send_file("my_fsm.png", mimetype="image/png")
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 8000)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
+# def movie():
+#     target_url = 'https://movies.yahoo.com.tw/'
+#     rs = requests.session()
+#     res = rs.get(target_url, verify=False)
+#     res.encoding = 'utf-8'
+#     soup = BeautifulSoup(res.text, 'html.parser')   
+#     content = ""
+#     for index, data in enumerate(soup.select('div.movielist_info h2 a')):
+#         if index == 10:
+#             return content       
+#         title = data.text
+#         link =  data['href']
+#         content += '{}\n{}\n'.format(title, link)
+#     return content
---
+++ fsm.py
@@ -0,0 +1,108 @@
+from transitions.extensions import GraphMachine
+import requests
+from bs4 import BeautifulSoup 
+from utils import send_text_message,send_image_url
+
+# depaat_code={
+#     "體育室":"A2","軍訓室":"A3","師培中心":"A4","計網中心":"A5","服務學習":"A6","共同英授":"AA","華語中心":"AH","不分系學程":"AN","外語中心":"A1","通識中心":"A9","公民歷史":"AG","基礎國文":"A7",
+#     "文學院學士班":"B0","中文系":"B1",
+# }
+class TocMachine(GraphMachine):
+    depart_code=""
+    course_name=""
+    state2_to_state1=False
+    show_fsm_to_user=False
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(model=self, **machine_configs)
+
+    def is_going_to_user(self,event):
+        text = event.message.text
+        return text.lower() == "restart"
+    
+    def on_enter_user(self,event):
+        if(self.show_fsm_to_user):
+            print("I'm entering user from show_fsm")
+            self.show_fsm_to_user=False
+            return
+        print("I'm entering user from state1")
+        reply_token = event.reply_token
+        send_text_message(reply_token, "請輸入系所代號 or show fsm")
+    def on_exit_user(self,event=None):
+        print("leaving user")
+    
+    def is_going_to_state1(self, event):
+        text = event.message.text
+        self.depart_code=text
+        if(text!="show fsm"):
+            return True
+    def on_enter_state1(self, event):
+        if(self.state2_to_state1):
+            print("I'm back from state2")
+            self.state2_to_state1=False
+            return
+        print("I'm entering state1 from user")
+        reply_token = event.reply_token
+        send_text_message(reply_token, "請輸入課程名稱 ")
+    def on_exit_state1(self,event=None):
+        print("Leaving state1")
+  
+    def is_going_to_state2(self, event):
+        text = event.message.text
+        self.course_name=text
+        if(text!="restart"):
+            return True
+        else:
+            return False
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
+        reply_token = event.reply_token
+
+        url="http://course-query.acad.ncku.edu.tw/qry/qry001.php?dept_no="+self.depart_code
+        res=requests.get(url)
+        res.encoding='utf-8'
+
+        soup=BeautifulSoup(res.text,"html.parser")
+        temp=[]
+        for link in soup.find_all("tr"):
+            #print(link.find("span","dept_name").text)
+            temp2=[]
+            coures=link.find_all("td")
+            for info in coures:
+                #print(info.text)
+                if(info.text!=""):
+                    temp2.append(info.text.replace("\n","").replace("\t",""))
+                else:
+                    temp2.append(" ")
+            href=link.find("a" ,"course_full_name")
+            if(href):
+                #print(href['href'])
+                temp2.append(href['href'])
+            else:
+                temp2.append("no course website")
+            temp.append(temp2)
+        result=""
+        for i in temp:
+            if(len(i)>13):
+                if(i[11]==self.course_name):
+                    result+=i[11]+"\n"+"教師名稱: "+i[14]+"\n"+"系號:"+i[1]+"序號:"+i[2]+"\n"+"學分數:"+i[13]+"\n"+"時間: "+i[17]+"\n"+"已選課人數:"+i[15]+" 餘額:"+i[16]+"\n"+"課程地圖連結:\n"+i[26]+"\n"
+        print(result)
+        if(result):
+            send_text_message(reply_token,result)
+        else:
+            send_text_message(reply_token,"查無此課程")
+        self.state2_to_state1=True
+        self.go_back_state1(event)
+    def on_exit_state2(self,event=None):
+        print("Leaving state2")
+
+    def is_going_to_show_fsm(self,event):
+        text = event.message.text
+        return text.lower() == "show fsm"
+    def on_enter_show_fsm(self,event):
+        print("I'm entering show fsh")
+        reply_token = event.reply_token
+        send_image_url(reply_token, "https://f74064070toc.herokuapp.com/show-fsm")
+        self.show_fsm_to_user=True
+        self.go_back_user(event)
+    def on_exit_show_fsm(self,event=None):
+        print("exit show_fsm")
